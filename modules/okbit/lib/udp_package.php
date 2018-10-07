0939cb3c33401d181a32ae9938bd7d8c2837da8d
SmarHome <foxvlad@yandex.ru>
2018-10-07 22:01:30 +0300

    Косяк с обработкой

--- modules/okbit/lib/udp_package.php
+++ modules/okbit/lib/udp_package.php
@@ -73,7 +73,7 @@
 			
 				if ($this->config['API_LOG_DEBMES']) {
 					DebMes(date("H:i:s") . " запуск функции присвоения значениея свойства объекта " . PHP_EOL, 'okbit');
-				
+				}
 			
 				$cmd_gate = SQLSelectOne("SELECT * FROM `okbit_gate` WHERE IP='".DBSafe($gate_ip)."'");
 						
