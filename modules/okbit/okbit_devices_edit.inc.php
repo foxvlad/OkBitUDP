158694c1c655ab7383ce93b4cb0819636a36daf2
SmarHome <foxvlad@yandex.ru>
2018-10-10 23:37:05 +0300

    testing

--- modules/okbit/okbit_devices_edit.inc.php
+++ modules/okbit/okbit_devices_edit.inc.php
@@ -5,7 +5,7 @@
 * @package project
 * @author Wizard <foxvlad@yandex.ru>
 * @copyright http://okbit.ru (c)
-* @version 0.1 (wizard, [Feb 04, 2018])
+* @version 0.1 (wizard, [Feb 14, 2018])
 */
 
 if ($this->owner->name == 'panel') {
